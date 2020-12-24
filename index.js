3d159fb6f99ed338066e69c979f6e76a0e06b92d
Towfeeque Ahmed <statowfiq@gmail.com>
2020-12-24 17:24:09 +0530

    added notification and tokens api

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-require("./models/db");
-const express =  require("express");
+const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 3000;
+require("./db");
 const bodyparser = require("body-parser");
-const friendsController =  require("./controllers/friends");
-const myfriendsController =  require("./controllers/myfriends");
-const morgan = require("morgan");
+const registration = require("./feature/Registration/registration.controller");
+const fcmToken = require("./feature/FcmToken/fcmToken.controller");
+const notifications = require("./feature/Notifications/notification");
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
-app.use(morgan("dev"));
-app.use("/friends", friendsController);
-app.use("/myfriends", myfriendsController);
+
+app.use("/api/registration", registration);
+app.use("/api/token", fcmToken);
+app.use("/api/notification", notifications);
 
 //Error handling
-app.use((req,res,next)=>{
-    const error = new Error("Not found");
-    error.status = 404;
-    next( error )
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
 });
 
-app.use((error,req,res,next)=>{
-    res.status(error.status || 500);
-    res.json({error:{message:error.message}})
-})
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: { message: error.message } });
+});
 
-app.listen(4000, ()=> console.log("listening in 4000"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-
+app.listen(port, () => {
+  console.info(`App is listening in http://localhost:${port}`);
+});
